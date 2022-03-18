ecebf65f5643bf53c667d961e9c2c85946092f3d
Regan Little <regan@dribbble.com>
2022-03-17 18:03:34 -0700

    add basic components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [list, setList] = useState([]);
+  const [url, setUrl] = useState(API_ENDPOINT);
+
+  useEffect(() => {
+    const fetchStories = async () => {
+      const request = await fetch(url);
+      const response = await request.json();
+      setList(response.hits);
+    }
+    fetchStories();
+  }, [url]);
+
+  const handleSearchInput = (e) =>
+    setSearchTerm(e.target.value);
+
+  const handelSearchSubmit = () =>
+    setUrl(`${API_ENDPOINT}${searchTerm}`);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <main>
+      <InputWithLabel onChange={handleSearchInput}>Search:</InputWithLabel>
+      <button type="submit" onClick={handelSearchSubmit}>Submit</button>
+      <List list={list} />
+    </main>
+  );
+}
+
+function List({list}) {
+  return (
+    <ul>
+      {list.map((item) =>
+        <ListItem {...item} key={item.objectID} />
+      )}
+    </ul>
+  );
+}
+
+function ListItem({title, url, ...rest}) {
+  console.log(rest)
+  return (
+    <li><a href={url}>{title}</a></li>
+  );
+}
+
+
+function InputWithLabel({type, label, placeholder = '', value, onChange}) {
+  return (
+    <div>
+      <label>{label}</label>
+      <input type={type} value={value} placeholder={placeholder} onChange={onChange} />
     </div>
   );
 }
